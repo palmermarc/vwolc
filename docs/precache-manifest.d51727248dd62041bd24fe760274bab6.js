8d9e206a69b0d9bbade045039698cbe7df1cdc7d
Marc Palmer <palmermarc@gmail.com>
2018-12-28 08:38:13 -0600

    Pushing a new build

---
+++ docs/precache-manifest.d51727248dd62041bd24fe760274bab6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b14043ba0e5153b4f405b345725a1c4",
+    "url": "/vwolc/static/media/Logo.4b14043b.svg"
+  },
+  {
+    "revision": "99ab24e4160b5b55481f",
+    "url": "/vwolc/static/js/runtime~main.99ab24e4.js"
+  },
+  {
+    "revision": "9576e1e9a2e0e61f8493",
+    "url": "/vwolc/static/js/main.9576e1e9.chunk.js"
+  },
+  {
+    "revision": "988e7ccad57398c9b65a",
+    "url": "/vwolc/static/js/1.988e7cca.chunk.js"
+  },
+  {
+    "revision": "9576e1e9a2e0e61f8493",
+    "url": "/vwolc/static/css/main.3ed51479.chunk.css"
+  },
+  {
+    "revision": "d8b3e3f74945c127f4277a015506b155",
+    "url": "/vwolc/index.html"
+  }
+];
