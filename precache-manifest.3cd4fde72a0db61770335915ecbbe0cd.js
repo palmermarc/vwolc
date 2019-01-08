50b6939b31f6b45692e4b394605a529c4cec1c0b
Marc Palmer <palmermarc@gmail.com>
2019-01-07 19:21:24 -0600

    Added the new build

---
+++ precache-manifest.3cd4fde72a0db61770335915ecbbe0cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b14043ba0e5153b4f405b345725a1c4",
+    "url": "/vwolc/static/media/Logo.4b14043b.svg"
+  },
+  {
+    "revision": "99ab24e4160b5b55481f",
+    "url": "/vwolc/static/js/runtime~main.99ab24e4.js"
+  },
+  {
+    "revision": "3c6f23782f5f3196c856",
+    "url": "/vwolc/static/js/main.3c6f2378.chunk.js"
+  },
+  {
+    "revision": "e8dca1d653107a04f917",
+    "url": "/vwolc/static/js/1.e8dca1d6.chunk.js"
+  },
+  {
+    "revision": "3c6f23782f5f3196c856",
+    "url": "/vwolc/static/css/main.2af16f8d.chunk.css"
+  },
+  {
+    "revision": "416b3e685bcdaacae055789f6c97f84c",
+    "url": "/vwolc/index.html"
+  }
+];
