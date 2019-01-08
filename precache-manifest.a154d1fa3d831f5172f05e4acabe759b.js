54802efb94dd8ca52361912365f519c31000b716
Marc Palmer <palmermarc@gmail.com>
2019-01-08 13:40:24 -0600

    Updates

--- precache-manifest.a154d1fa3d831f5172f05e4acabe759b.js
+++ precache-manifest.a154d1fa3d831f5172f05e4acabe759b.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/vwolc/static/js/runtime~main.99ab24e4.js"
   },
   {
-    "revision": "f21b0c036a753055ad91",
-    "url": "/vwolc/static/js/main.f21b0c03.chunk.js"
+    "revision": "957458724197c3d3e91f",
+    "url": "/vwolc/static/js/main.95745872.chunk.js"
   },
   {
     "revision": "30133a1cba60967e70df",
     "url": "/vwolc/static/js/1.30133a1c.chunk.js"
   },
   {
-    "revision": "f21b0c036a753055ad91",
+    "revision": "957458724197c3d3e91f",
     "url": "/vwolc/static/css/main.2af16f8d.chunk.css"
   },
   {
-    "revision": "8d8b3e9bff3df5ef7612264ad8070ca4",
+    "revision": "7ec4c38c457739fd2c77d61951f5f679",
     "url": "/vwolc/index.html"
   }
 ];
