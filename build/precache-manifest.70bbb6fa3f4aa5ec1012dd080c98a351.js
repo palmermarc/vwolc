06ca28d64d8ef5ec07c31bcebc8227138d3d4e5d
Marc Palmer <palmermarc@gmail.com>
2019-01-25 11:18:38 -0600

    Removed the build folder from gitignore

---
+++ build/precache-manifest.70bbb6fa3f4aa5ec1012dd080c98a351.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b14043ba0e5153b4f405b345725a1c4",
+    "url": "/static/media/Logo.4b14043b.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4d1138c82e2e396af6e8",
+    "url": "/static/js/main.4d1138c8.chunk.js"
+  },
+  {
+    "revision": "1e355509bc64a0cb6802",
+    "url": "/static/js/1.1e355509.chunk.js"
+  },
+  {
+    "revision": "4d1138c82e2e396af6e8",
+    "url": "/static/css/main.d4f5c672.chunk.css"
+  },
+  {
+    "revision": "4d366495924f67be53590d192a7b3dd5",
+    "url": "/index.html"
+  }
+];
